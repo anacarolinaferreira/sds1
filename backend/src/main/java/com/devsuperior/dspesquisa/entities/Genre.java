ecbac133a2a4c18238bd8476ffe92f7168d703b0
Ana Carolina Ferreira <souzacarol451@gmail.com>
2020-09-15 00:05:59 -0300

    POST records

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Genre.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Genre.java
@@ -14,9 +14,6 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_genre")
 public class Genre implements Serializable {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	@Id
